f7a9acf07d469f048d250aa8ce2e49e08291404e
saipratap99 <122005067@sastra.ac.in>
2020-09-12 10:49:11 +0530

    Book can be created , but validation not done for assigning author to book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,16 @@
 class BooksController < ApplicationController
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      render json: { status: "SUCCESS", message: "Book is added", data: @book }, status: :ok
+    else
+      render json: { status: "ERROR", message: "Book is not added", data: @book.erros }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :edition, :publication_year)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
   has_many :book_authors
   has_many :authors, through: :book_authors
+  validates :name, presence: true, uniqueness: true
+  validates :edition, :publication_year, presence: true
+  validates_associated :authors
 end
