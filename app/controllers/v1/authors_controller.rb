833f2d7b0cc79fb4a11e0f0513c81cdb8d779348
saipratap99 <122005067@sastra.ac.in>
2020-09-13 08:03:32 +0530

    Pagination done for authors

--- app/controllers/v1/authors_controller.rb
+++ app/controllers/v1/authors_controller.rb
@@ -1,8 +1,18 @@
 module V1
   class AuthorsController < ApplicationController
     def index
-      @authors = Author.all
-      render json: @authors
+      @authors = Author.page(page).per(per_page)
+      render json: { authors: @authors, meta: { pagination: { per_page: per_page, total_pages: @authors.total_count, current_page: page } } }
+    end
+
+    private
+
+    def page
+      @page = params[:page] || 1
+    end
+
+    def per_page
+      @per_page = params[:per_page] || 2
     end
   end
 end
