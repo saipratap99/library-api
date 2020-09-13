7c9a61a7cf2a920165ae0e38881db500a6f7f119
saipratap99 <122005067@sastra.ac.in>
2020-09-13 08:18:20 +0530

    Pagination total pages count fixed

--- app/controllers/v1/authors_controller.rb
+++ app/controllers/v1/authors_controller.rb
@@ -2,17 +2,22 @@ module V1
   class AuthorsController < ApplicationController
     def index
       @authors = Author.page(page).per(per_page)
-      render json: { authors: @authors, meta: { pagination: { per_page: per_page, total_pages: @authors.total_count, current_page: page } } }
+      render json: { authors: @authors, meta: { pagination: { per_page: per_page, total_pages: page_count, current_page: page } } }
     end
 
     private
 
     def page
-      @page = params[:page] || 1
+      @page ||= params[:page] || 1
     end
 
     def per_page
-      @per_page = params[:per_page] || 2
+      @per_page ||= params[:per_page] || 2
+    end
+
+    def page_count
+      count = Author.count / per_page.to_f
+      count.ceil()
     end
   end
 end
