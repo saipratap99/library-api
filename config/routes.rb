546335510699c3c0034b6600edfce0e6c673b3ff
saipratap99 <122005067@sastra.ac.in>
2020-09-14 08:47:04 +0530

    Changed the default host

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   namespace :v1 do
-    resources :book_authors
     resources :books
     resources :authors
   end
