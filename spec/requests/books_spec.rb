ce58b48fb16e3013ef315b9cf65c9cba0d9b72fa
saipratap99 <122005067@sastra.ac.in>
2020-09-14 08:22:21 +0530

    Modified GET test for books

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "/v1/books", type: :request do
           required: ["name", "edition","publication_year","authors"],
         }
         response "200", "Book created" do
-          let(:encounter) { { name: "Ruby Programming", edition: 1,publication_year: "2011", authors: "18,29,75" } }
+          let(:book) { { name: "Ruby Programming", edition: 1,publication_year: "2011", authors: "18,29,75" } }
           run_test!
         end
     end
@@ -23,16 +23,12 @@ RSpec.describe "/v1/books", type: :request do
     path "/v1/books" do
         get "Retrives Books" do
           tags "Books"
-          parameter name: :book, in: :body, schema: {
-          type: :object,
-          properties: {
-            page: { type: :integer },
-            per_page: { type: :integer },
-          },
-          required: ["page","per_page"],
-          }
+          produces "application/json"
+          parameter name: :page, in: :query,type: :integer
+          parameter name: :per_page, in: :query,type: :integer
           response "200", "Books retrived" do
-            let(:encounter){{page: 1,per_page: 5}}
+            let(:page){1}
+            let(:per_page){10}
             run_test!
           end
         end
